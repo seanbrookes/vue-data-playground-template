23204d88dbf1fec1dfcfc624fcc49e8c8dea44e3
seanbrookes <seanbrookes@beachair.ca>
2023-08-13 11:27:20 -0700

    cleanup and add theatre three and d3 libs

--- src/components/main/Main.spec.js
+++ src/components/main/Main.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5174');
+  await page.goto('http://localhost:7777');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Vue data playground template/);
